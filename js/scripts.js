fb94f1a5a4195dcde1b30a2a4753236d0a21f89d
= <=>
2017-06-05 15:58:59 -0700

    perform basic form functions.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var beverage = $("#beverage").val();
+var option = parseInt("beverage: ");
+var flavor = $("input:radio[name=flavor]:checked").val();
+var dob = $("#born").val();
+var favoriteColor = $("#color").val();
